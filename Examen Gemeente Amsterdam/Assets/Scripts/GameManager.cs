66239f23e3351650ac48864567a41f8abec764a6
Miguel-Heijmans <58071327+Miguel-Heijmans@users.noreply.github.com>
2023-04-05 12:44:22 +0200

    added timer, questions, win & lose conditions and "animations"
    added the timer, 37 questions, win and lose conditions, fixed a spelling mistake in the code, "animations", and questions can now be answered correctly or incorrectly.

--- Examen Gemeente Amsterdam/Assets/Scripts/GameManager.cs
+++ Examen Gemeente Amsterdam/Assets/Scripts/GameManager.cs
@@ -19,23 +19,74 @@ public class GameManager : MonoBehaviour
     private Text factText;
 
     [SerializeField]
+    private Text trueAnswerText;
+
+    [SerializeField]
+    private Text falseAnswerText;
+
+    [SerializeField]
+    private Animator animator;
+
+
+    [SerializeField]
+    private Text TimerText;
+
+    public float TimeLeft;
+    public bool TimerOn = false;
+
     private float timeBetweenQuestions = 1f;
 
+    private int correctAnswers = 0;
+
+    
     
 
     /*void Awake() {
         DontDestroyOnLoad(transform.gameObject);
     }*/
 
+    
+    
 
     private void Start()
     {
+
+        TimerOn = true;
+
         if (unansweredQuestions == null || unansweredQuestions.Count == 0)
         {
             unansweredQuestions = questions.ToList<Question>();
         }
+        GetComponent<Timer>();
 
         SetCurrentQuestion();
+    }
+
+    
+    private void Update()
+    {
+
+        if (TimerOn)
+        {
+            if (TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+                updateTimer(TimeLeft);
+            }else
+            {
+                TimeLeft = 0;
+                TimerOn = false;
+            }
+            
+        }
+
+        if (correctAnswers == 20)
+        {
+            Win();
+        }else if (TimeLeft <= 0 && correctAnswers < 20)
+        {
+            Lose();
+        }
     }
 
     void SetCurrentQuestion()
@@ -46,6 +97,18 @@ public class GameManager : MonoBehaviour
         factText.text = currentQuestion.fact;
 
         unansweredQuestions.RemoveAt(randomQuestionIndex);
+
+        if (currentQuestion.isTrue)
+        {
+            trueAnswerText.text = "CORRECT!";
+            falseAnswerText.text = "FALSE!";
+
+        }else
+        {
+            trueAnswerText.text = "WRONG!";
+            falseAnswerText.text = "CORRECT!";
+        }
+
     }
 
     IEnumerator TransitionToNextQuestion()
@@ -59,8 +122,11 @@ public class GameManager : MonoBehaviour
 
     public void UserSelectTrue()
     {
+
+        animator.SetTrigger("True");
         if (currentQuestion.isTrue)
         {
+            correctAnswers += 1;
             Debug.Log("CORRECT!");
         }else
         {
@@ -72,14 +138,36 @@ public class GameManager : MonoBehaviour
 
     public void UserSelectFalse()
     {
+        animator.SetTrigger("False");
         if (currentQuestion.isTrue)
         {
+            correctAnswers += 1;
             Debug.Log("CORRECT!");
         }else
         {
             Debug.Log("WRONG!");
         }
         StartCoroutine(TransitionToNextQuestion());
+    }
+
+    private void Win()
+    {
+        
+    }
+
+    private void Lose()
+    {
+        
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        TimerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
     }
     
 }
